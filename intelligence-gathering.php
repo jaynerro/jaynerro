1e6377c644e6c266b73de492ac66b3771d116f8f
jaynerro <65121226+jaynerro@users.noreply.github.com>
2023-04-27 15:59:10 +0200

    :build: try something with intelligence gathering

--- intelligence-gathering.php
+++ intelligence-gathering.php
@@ -32,10 +32,54 @@
     
     <div class="container" style="margin-top: 80px;">
       <div class="row justify-content-center">
-        <div class="col">
+        <div class="col-sm-8">
           <h1> 
             Veille informationnelle
           </h1>
+        
+          <div class="row row-cols-1 row-cols-sm-3 g-4" style="margin-top: 40px;">
+            <div class="col">
+              <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text" style="color: black">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <a href="#" class="position-absolute top-80 start-10 btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text" style="color: black">This is a short card.</p>
+                  <a href="#" class="position-absolute top-80 start-10 btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text" style="color: black">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="position-absolute top-80 start-10 btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text" style="color: black">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <a href="#" class="position-absolute top-80 start-10 btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
